b99e07256d50c474645731ba5932568b6ddf5055
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-08 11:41:16 +0800

    add MyDate and hello.md

---
+++ MyDate/src/com/chenyun/date/StudentEntity.java
@@ -0,0 +1,66 @@
+package com.chenyun.date;
+
+import javax.persistence.*;
+
+/**
+ * Created by thomas.wang on 2016/12/6.
+ */
+@Entity
+@Table(name = "student", schema = "acess", catalog = "")
+public class StudentEntity {
+    private int id;
+    private String age;
+    private String name;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "age")
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StudentEntity that = (StudentEntity) o;
+
+        if (id != that.id) return false;
+        if (age != null ? !age.equals(that.age) : that.age != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
---
+++ MyDate/src/com/chenyun/date/Test.java
@@ -0,0 +1,11 @@
+package com.chenyun.date;
+
+/**
+ * Created by thomas.wang on 2016/12/6.
+ */
+public class Test {
+    public static void main(String[] args){
+//            Main mains = new Main();
+            System.out.print("aaaaaaaaa");
+    }
+}
