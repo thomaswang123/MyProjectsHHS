76efd4deaf3b7b257ff274091f618568c96debd6
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-09 18:45:22 +0800

    pull webshoppring

---
+++ Webshoppring/out/artifacts/Web_war_exploded/WEB-INF/dao/AdminfoEtDao.java
@@ -0,0 +1,12 @@
+package com.chenyun.dao;
+
+import com.chenyun.entity.AdminfoEt;
+
+import java.util.ArrayList;
+
+/**
+ * Created by thomas.wang on 2016/12/9.
+ */
+public interface AdminfoEtDao {
+     ArrayList<AdminfoEt>  findAll();
+}
